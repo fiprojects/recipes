072fcfde1b23962338559a17aa2678779899a6a3
Jozef Živčic <jozefzivcic@gmail.com>
2017-10-31 18:05:29 +0100

    Login page

---
+++ RecipesWeb/Controllers/LoginController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RecipesWeb.Controllers
+{
+    public class LoginController : Controller
+    {
+        public LoginController()
+        {
+            
+        }
+
+        public IActionResult Show()
+        {
+            return View();
+        }
+        
+    }
+}
