d2bf98baefe49603bafda0a8183728eb0b68aa42
Jozef Živčic <jozefzivcic@gmail.com>
2017-11-03 11:58:43 +0100

    Sign up page, added reference to this page

---
+++ RecipesWeb/Controllers/SignUpController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RecipesCore.Services;
+
+namespace RecipesWeb.Controllers
+{
+    public class SignUpController : Controller
+    {
+        private readonly IUserService _userService;
+
+        public SignUpController(IUserService userService)
+        {
+            _userService = userService;
+        }
+        
+        public IActionResult Show()
+        {
+            return View();
+        }
+
+        public IActionResult SignUp()
+        {
+            return Redirect("/");
+        }
+    }
+}
