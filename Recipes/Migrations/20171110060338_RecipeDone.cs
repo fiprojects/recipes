ac44afd0dfe2cce8401f7194f8aa1965fae39a35
veronikaaksamit <veronika.aksamitova@gmail.com>
2017-11-11 13:48:51 +0100

    migrations for relationship tables Seen, Done, Ratings

---
+++ Recipes/Migrations/20171110060338_RecipeDone.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace RecipesCore.Migrations
+{
+    public partial class RecipeDone : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "RecipeDone",
+                columns: table => new
+                {
+                    RecipeId = table.Column<long>(type: "int8", nullable: false),
+                    UserId = table.Column<long>(type: "int8", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RecipeDone", x => new { x.RecipeId, x.UserId });
+                    table.ForeignKey(
+                        name: "FK_RecipeDone_Recipes_RecipeId",
+                        column: x => x.RecipeId,
+                        principalTable: "Recipes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_RecipeDone_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RecipeDone_UserId",
+                table: "RecipeDone",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RecipeDone");
+        }
+    }
+}
