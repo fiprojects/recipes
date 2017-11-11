2ce51169bd665a94604fe0877697dd8d4af897a0
veronikaaksamit <veronika.aksamitova@gmail.com>
2017-11-11 22:22:38 +0100

    added ratingService to RecipesController, changed Show method + added recipeRating for current user

--- RecipesWeb/Controllers/RecipesController.cs
+++ RecipesWeb/Controllers/RecipesController.cs
@@ -1,21 +1,41 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using RecipesCore.Models;
 using RecipesCore.Services;
+using RecipesWeb.ViewModels;
 
 namespace RecipesWeb.Controllers
 {
     public class RecipesController : Controller
     {
         private readonly IRecipesService _recipesService;
+        private readonly IRatingService _ratingService;
 
-        public RecipesController(IRecipesService recipesService)
+        public RecipesController(IRecipesService recipesService, IRatingService ratingService)
         {
+            _ratingService = ratingService;
             _recipesService = recipesService;
         }
 
         public IActionResult Show(long id)
         {
-            var recipe = _recipesService.Get(id);
-            return View(recipe);
+            RecipeRatings rating = new RecipeRatings();
+            var userName = HttpContext.User.Identity.Name;
+            if (userName != null)
+            {
+                var userRating = _ratingService.GetByUserNameAndRecipeId(userName, id);
+                if (userRating != null)
+                    rating = userRating;
+            }
+            
+            var viewModel = new RecipesShowModel()
+            {
+                Recipe = _recipesService.Get(id),
+                RecipeUserRating = rating
+            };
+
+            return View(viewModel);
         }
     }
 }
