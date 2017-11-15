7a16a58870a763aff7e894cfe13c780a6da6e917
veronikaaksamit <veronika.aksamitova@gmail.com>
2017-11-15 13:39:44 +0100

    added service for WordStatistics ...graphs of word freq

---
+++ Recipes/Services/IWordStatisticsService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecipesCore.Services
+{
+    public interface IWordStatisticsService
+    {
+        List<Tuple<string, int>> GetCookingTimeAndFreq();
+    }
+}
