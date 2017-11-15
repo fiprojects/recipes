9f8fb0dc6ccbf38917718e87a1701f4ca65e21ac
veronikaaksamit <veronika.aksamitova@gmail.com>
2017-11-15 13:39:44 +0100

    added service for WordStatistics ...graphs of word freq

---
+++ Recipes/Services/WordStatisticsService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RecipesCore.Models;
+
+namespace RecipesCore.Services
+{
+    public class WordStatisticsService : IWordStatisticsService
+    {
+        private readonly RecipesContext _db;
+
+        public WordStatisticsService(RecipesContext db)
+        {
+            if (db != null)
+                _db = db;
+        }
+
+        public List<Tuple<string, int>> GetCookingTimeAndFreq()
+        {
+            List<Tuple<string, int>> cookTimes =  new List<Tuple<string, int>>();
+            List<Recipe> recipes = _db.Recipes.Select(a => a).ToList();
+            foreach (Recipe r in recipes)
+            {
+                
+            }
+            return cookTimes;
+        }
+    }
+}
