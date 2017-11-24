58339257a00da5767cccc2804b8db72b2a42a783
Jozef Živčic <jozefzivcic@gmail.com>
2017-11-24 18:59:10 +0100

    TfIdfModel, Service for storing it

---
+++ Recipes/Models/TfIdfModel.cs
@@ -0,0 +1,13 @@
+﻿namespace RecipesCore.Models
+{
+    public class TfIdfModel
+    {
+        public long Id { get; set; }
+        
+        public Recipe Recipe { get; set; }
+        
+        public string Term { get; set; }
+        
+        public double TfIdf { get; set; }
+    }
+}
---
+++ Recipes/Services/ITfIdfService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using RecipesCore.Models;
+
+namespace RecipesCore.Services
+{
+    public interface ITfIdfService
+    {
+        void Add(List<TfIdfModel> models);
+        
+        void Add(TfIdfModel model);
+    }
+}
