3900907cdec96ca0a6b253356788eac295fbb7da
Jozef Živčic <jozefzivcic@gmail.com>
2017-11-25 14:34:14 +0100

    Tests on GetTermsWithCountForRecipe

--- RecipesTests/TfIdfTest.cs
+++ RecipesTests/TfIdfTest.cs
@@ -1,21 +1,145 @@
-﻿using NFluent;
+﻿using System.Collections.Generic;
+using System.IO;
+using Moq;
+using NFluent;
 using NUnit.Framework;
+using RecipesCore.Models;
+using RecipesCore.Processors;
+using RecipesCore.Services;
 
 namespace RecipesTests
 {
     [TestFixture]
     public class TfIdfTest
     {
-        [Test]
-        public void BasicTest()
+        private TfIdfComputer _tfIdfComputer;
+        
+        private Recipe _recipe1;
+
+        private Recipe _recipe2;
+
+        [SetUp]
+        public void Init()
         {
-            Assert.AreEqual(15, 15);
+            InitRecipes();
+            var recipesMock = new Mock<IRecipesService>();
+            recipesMock.Setup(service => service.GetAll()).Returns(new List<Recipe>{_recipe1, _recipe2});
+            var tfIdfMock = new Mock<ITfIdfService>();
+            _tfIdfComputer = new TfIdfComputer(recipesMock.Object, tfIdfMock.Object);
+            _tfIdfComputer = new TfIdfComputer(recipesMock.Object, tfIdfMock.Object);
+
+        }
+
+        private void InitRecipes()
+        {
+            _recipe1 = new Recipe
+            {
+                Directions= "\r\nRecipe test A "
+            };
+            _recipe2 = new Recipe();
+            string readFromFile = File.ReadAllText("../../../SampleDirections/recipe2.txt");
+            _recipe2.Directions = readFromFile;
         }
 
         [Test]
-        public void NFluentTest()
+        public void NoEmptyStringInDictTest()
         {
-            Check.That(15).Equals(15);
+            var r = new Recipe
+            {
+                Directions = "something and ."
+            };
+            var dict = new Dictionary<string, int>();
+            dict["something"] = 1;
+            dict["and"] = 1;
+            var ret = _tfIdfComputer.GetTermsWithCountForRecipe(r);
+            CollectionAssert.AreEquivalent(dict, ret);
+        }
+
+        [Test]
+        public void TrimCommaTest()
+        {
+            var r = new Recipe
+            {
+                Directions = "something and,"
+            };
+            var dict = new Dictionary<string, int>();
+            dict["something"] = 1;
+            dict["and"] = 1;
+            var ret = _tfIdfComputer.GetTermsWithCountForRecipe(r);
+            Assert.False(dict.ContainsKey("and,"));
+            CollectionAssert.AreEquivalent(dict, ret);
+        }
+        
+        [Test]
+        public void TrimPeriodTest()
+        {
+            var r = new Recipe
+            {
+                Directions = "something and. "
+            };
+            var dict = new Dictionary<string, int>();
+            dict["something"] = 1;
+            dict["and"] = 1;
+            var ret = _tfIdfComputer.GetTermsWithCountForRecipe(r);
+            Assert.False(dict.ContainsKey("and."));
+            CollectionAssert.AreEquivalent(dict, ret);
+        }
+        
+        [Test]
+        public void TrimSemicolonTest()
+        {
+            var r = new Recipe
+            {
+                Directions = "something and; "
+            };
+            var dict = new Dictionary<string, int>();
+            dict["something"] = 1;
+            dict["and"] = 1;
+            var ret = _tfIdfComputer.GetTermsWithCountForRecipe(r);
+            Assert.False(dict.ContainsKey("and;"));
+            CollectionAssert.AreEquivalent(dict, ret);
+        }
+        
+        [Test]
+        public void TermsInLowerCaseTest()
+        {
+            var r = new Recipe
+            {
+                Directions = "Something aNd something"
+            };
+            var dict = new Dictionary<string, int>();
+            dict["something"] = 2;
+            dict["and"] = 1;
+            var ret = _tfIdfComputer.GetTermsWithCountForRecipe(r);
+            CollectionAssert.AreEquivalent(dict, ret);
+        }
+        
+        [Test]
+        public void AllSpacesRemovedTest()
+        {
+            var r = new Recipe
+            {
+                Directions = "something  and    something"
+            };
+            var dict = new Dictionary<string, int>();
+            dict["something"] = 2;
+            dict["and"] = 1;
+            var ret = _tfIdfComputer.GetTermsWithCountForRecipe(r);
+            CollectionAssert.AreEquivalent(dict, ret);
+        }
+        
+        [Test]
+        public void TermsInLowerCaseTestTest()
+        {
+            var r = new Recipe
+            {
+                Directions = "something \r\n and something \r\n\r\n and \n something"
+            };
+            var dict = new Dictionary<string, int>();
+            dict["something"] = 3;
+            dict["and"] = 2;
+            var ret = _tfIdfComputer.GetTermsWithCountForRecipe(r);
+            CollectionAssert.AreEquivalent(dict, ret);
         }
     }
 }
