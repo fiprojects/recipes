9a28cc15a24cfb82db698c5fd2878ce3275a85a6
Jozef Živčic <jozefzivcic@gmail.com>
2017-11-29 13:27:17 +0100

    New project for running TF-IDF on database

---
+++ TfIdfConsole/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TfIdfConsole
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
