2bf3e11e6e64a0b4ff70931bad883779be67855b
Jozef Živčic <jozefzivcic@gmail.com>
2017-12-01 19:07:57 +0100

    TfIdf GetAll and test class for TfIdfService

--- Recipes/Services/TfIdfService.cs
+++ Recipes/Services/TfIdfService.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using RecipesCore.Migrations;
 using RecipesCore.Models;
 
 namespace RecipesCore.Services
@@ -26,6 +28,22 @@ namespace RecipesCore.Services
                 return;
             _db.Add(model);
             _db.SaveChanges();
+        }
+
+        public List<TfIdfModel> GetAll()
+        {
+            return _db.TfIdfModels.ToList();
+        }
+
+        public List<TfIdfModel> GetAllExcept(TfIdfModel model)
+        {
+            var list = GetAll();
+            return list.Where(x => !x.Equals(model)).ToList();
+        }
+        
+        public List<TfIdfModel> GetSimilarRecipes(TfIdfModel model)
+        {
+            return null;
         }
     }
 }
---
+++ RecipesTests/TfIdfServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using RecipesCore;
+using RecipesCore.Services;
+
+namespace RecipesTests
+{
+    [TestFixture]
+    public class TfIdfService
+    {
+        private ITfIdfService _tfIdfService;
+
+        [SetUp]
+        public void Init()
+        {
+            var db = new RecipesContext();
+            _tfIdfService = new TfIdfService(db);
+        }
+    }
+}
