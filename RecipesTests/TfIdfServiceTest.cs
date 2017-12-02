8f00adb3ccbcfefff61eee89fdb3a7c79f8aeaa0
Jozef Živčic <jozefzivcic@gmail.com>
2017-12-02 15:53:25 +0100

    Added another model to tests

--- RecipesTests/TfIdfServiceTest.cs
+++ RecipesTests/TfIdfServiceTest.cs
@@ -19,7 +19,14 @@ namespace RecipesTests
         private TfIdfModel _model2;
 
         private TfIdfModel _model3;
+
+        private TfIdfModel _model4;
+        
         private double _threshold = 1e-10;
+        private Recipe _recipe1;
+        private Recipe _recipe2;
+        private Recipe _recipe3;
+        private Recipe _recipe4;
 
         // this method is inspired by https://stackoverflow.com/questions/34332761/mocking-dbsett-inline
         private DbSet<T> ToDbSet<T>(List<T> sourceList) where T : class
@@ -38,6 +45,7 @@ namespace RecipesTests
         [SetUp]
         public void Init()
         {
+            InitRecipes();
             InitTfIdfModels();
             var modelsList = new List<TfIdfModel> {_model1, _model2, _model3};
             var mock = new Mock<RecipesContext>();
@@ -45,14 +53,31 @@ namespace RecipesTests
             _tfIdfService = new TfIdfService(mock.Object);
         }
 
+        private void InitRecipes()
+        {
+            _recipe1 = new Recipe
+            {
+                Description = "basil soup"
+            };
+            _recipe2 = new Recipe
+            {
+                Description = "egg"
+            };
+            _recipe3 = new Recipe
+            {
+                Description = "soup with egg and basil"
+            };
+            _recipe4 = new Recipe
+            {
+                Description = "egg on bread"
+            };
+        }
+
         private void InitTfIdfModels()
         {
             _model1 = new TfIdfModel
             {
-                Recipe = new Recipe
-                {
-                    Description = "basil soup"
-                }
+                Recipe = _recipe1
             };
             _model1.Elements.Add(new TfIdfElement{Term = "a", TfIdf = 0.054});
             _model1.Elements.Add(new TfIdfElement{Term = "soup", TfIdf = 0.103});
@@ -61,10 +86,7 @@ namespace RecipesTests
             
             _model2 = new TfIdfModel
             {
-                Recipe = new Recipe
-                {
-                    Description = "tasty egg"
-                }
+                Recipe = _recipe2
             };
             _model2.Elements.Add(new TfIdfElement{Term = "egg", TfIdf = 2.4});
             _model2.Elements.Add(new TfIdfElement{Term = "with", TfIdf = 0.004});
@@ -74,16 +96,23 @@ namespace RecipesTests
             
             _model3 = new TfIdfModel
             {
-                Recipe = new Recipe
-                {
-                    Description = "everything together"
-                }
+                Recipe = _recipe3
             };
             _model3.Elements.Add(new TfIdfElement{Term = "mix", TfIdf = 1.32});
             _model3.Elements.Add(new TfIdfElement{Term = "egg", TfIdf = 2.5});
             _model3.Elements.Add(new TfIdfElement{Term = "soup", TfIdf = 0.42});
             _model3.Elements.Add(new TfIdfElement{Term = "and", TfIdf = 0.0048});
             _model3.Elements.Add(new TfIdfElement{Term = "basil", TfIdf = 1.278424});
+
+            _model4 = new TfIdfModel
+            {
+                Recipe = _recipe4
+            };
+            _model4.Elements.Add(new TfIdfElement{Term = "egg", TfIdf = 2.7});
+            _model4.Elements.Add(new TfIdfElement{Term = "with", TfIdf = 0.0035});
+            _model4.Elements.Add(new TfIdfElement{Term = "bread", TfIdf = 1.72});
+            _model4.Elements.Add(new TfIdfElement{Term = "and", TfIdf = 0.00034});
+            _model4.Elements.Add(new TfIdfElement{Term = "salt", TfIdf = 1.0456789});
         }
 
         [Test]
@@ -129,12 +158,36 @@ namespace RecipesTests
             var similarity = _tfIdfService.ComputeCosineSimilarity(_model1, _model3);
             Assert.That(similarity, Is.EqualTo(expectedSim).Within(_threshold));
         }
+
+        [Test]
+        public void ComputeSimilarityBetweenOneAndFourTest()
+        {
+            double expectedSim = 1.0;
+            var similarity = _tfIdfService.ComputeCosineSimilarity(_model1, _model4);
+            Assert.That(similarity, Is.EqualTo(expectedSim).Within(_threshold));
+        }
         
         [Test]
         public void ComputeSimilarityBetweenTwoAndThreeTest()
         {
             double expectedSim = 0.9999999867;
             var similarity = _tfIdfService.ComputeCosineSimilarity(_model2, _model3);
+            Assert.That(similarity, Is.EqualTo(expectedSim).Within(_threshold));
+        }
+
+        [Test]
+        public void ComputeSimilarityBetweenTwoAndFourTest()
+        {
+            double expectedSim = 0.9991415764;
+            var similarity = _tfIdfService.ComputeCosineSimilarity(_model2, _model4);
+            Assert.That(similarity, Is.EqualTo(expectedSim).Within(_threshold));
+        }
+        
+        [Test]
+        public void ComputeSimilarityBetweenThreeAndFourTest()
+        {
+            double expectedSim = 0.9999983907;
+            var similarity = _tfIdfService.ComputeCosineSimilarity(_model3, _model4);
             Assert.That(similarity, Is.EqualTo(expectedSim).Within(_threshold));
         }
         
