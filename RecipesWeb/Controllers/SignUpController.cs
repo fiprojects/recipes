64ac54c2252ac4a21629a44e58f097d6664fc107
Michael Le <mail@michael-le.net>
2017-12-05 12:00:12 +0100

    User Allergies Checkbox

--- RecipesWeb/Controllers/SignUpController.cs
+++ RecipesWeb/Controllers/SignUpController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
@@ -6,26 +7,31 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 using RecipesCore.Models;
 using RecipesCore.Services;
+using RecipesWeb.ViewModels;
 
 namespace RecipesWeb.Controllers
 {
     public class SignUpController : Controller
     {
         private readonly IUserService _userService;
+        private readonly IIngredientService _ingredientService;
 
-        public SignUpController(IUserService userService)
+        public SignUpController(IUserService userService, IIngredientService ingredientService)
         {
             _userService = userService;
+            _ingredientService = ingredientService;
         }
         
         public IActionResult Show()
         {
-            return View();
+            return View(PrepareViewModel());
         }
 
         [HttpPost]
-        public async Task<IActionResult> SignUp(string user_login, string vegetarian, string vegan, string allergies)
+        public async Task<IActionResult> SignUp(string user_login, string vegetarian, string vegan, string allergies,
+            [Bind] RegisterViewModel registerViewModel)
         {
+            var ingredients = _ingredientService.GetAll();
             var username = user_login;
             if (string.IsNullOrWhiteSpace(username) || _userService.Exists(user_login))
             {
@@ -34,7 +40,11 @@ namespace RecipesWeb.Controllers
             
             bool bVegetarian = vegetarian?.Equals("on") ?? false;
             bool bVegan = vegan?.Equals("on") ?? false;
-            List<UserAllergie> allergiesList = UserAllergie.GetUserAllergiesFromString(allergies);
+            var allergiesList = registerViewModel.Ingredients
+                .Where(i => i.Selected)
+                .Select(i => ingredients.First(j => j.Id == i.Id))
+                .Select(i => new UserAllergie(i))
+                .ToList();
             User user = new User(username, bVegetarian, bVegan, allergiesList);
             _userService.Add(user);
             
@@ -51,5 +61,15 @@ namespace RecipesWeb.Controllers
             return Redirect("/");
         }
 
+        private RegisterViewModel PrepareViewModel()
+        {
+            var viewModel = new RegisterViewModel();
+            _ingredientService.GetAll().ForEach(i =>
+            {
+                viewModel.Ingredients.Add(new SelectedItemViewModel { Id = i.Id, Name = i.Name });
+            });
+
+            return viewModel;
+        }
     }
 }
