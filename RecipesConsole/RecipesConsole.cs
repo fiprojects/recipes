53fe38e87d8f8ae8c3704bea6898d4bdabf93bbe
Michael Le <mail@michael-le.net>
2017-12-06 23:54:04 +0100

    Unified recipes console

--- RecipesConsole/RecipesConsole.cs
+++ RecipesConsole/RecipesConsole.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.IO;
+using System.Reflection;
+using System.Xml;
+using Crawler;
+using log4net;
 using Microsoft.Extensions.DependencyInjection;
 using RecipesCore;
 using RecipesCore.Services;
@@ -9,12 +13,16 @@ namespace RecipesConsole
 {
     public class RecipesConsole
     {
+        private static readonly ILog Log = LogManager.GetLogger(typeof(Program));
         private readonly IServiceProvider _serviceProvider;
 
         public RecipesConsole()
         {
+            SetupLogging();
+
             IServiceCollection serviceCollection = new ServiceCollection();
             serviceCollection.AddRecipesCore();
+            serviceCollection.AddDbContext<RecipesContext>();
             _serviceProvider = serviceCollection.BuildServiceProvider();
         }
 
@@ -24,6 +32,10 @@ namespace RecipesConsole
             {
                 case "build":
                     Build(args);
+                    break;
+
+                case "crawl":
+                    Crawl();
                     break;
 
                 default:
@@ -54,14 +66,26 @@ namespace RecipesConsole
             }
         }
 
+        public void Crawl()
+        {
+            var recipesService = _serviceProvider.GetService<IRecipesService>();
+
+            ICrawler crawler = new AllRecipesCrawler { Logger = Log };
+            crawler.ProcessRecipes(RecipeIdProvider.CategorizedCsv("recipes.csv"), recipesService.Add);
+        }
+
+        private static void SetupLogging()
+        {
+            var config = new XmlDocument();
+            config.Load(File.OpenRead("log4net.config"));
+
+            var repository = LogManager.CreateRepository(Assembly.GetEntryAssembly(), typeof(log4net.Repository.Hierarchy.Hierarchy));
+            log4net.Config.XmlConfigurator.Configure(repository, config["log4net"]);
+        }
+
         private static string GetArgument(IReadOnlyList<string> args, int index)
         {
-            if (index >= args.Count)
-            {
-                return null;
-            }
-
-            return args[index].ToLower();
+            return index < args.Count ? args[index].ToLower() : null;
         }
 
         private static void GetArgument(IReadOnlyList<string> args, int index, out string value)
