77ec9a669bccd4766213ea0c42d84446436a3cc6
veronikaaksamit <veronika.aksamitova@gmail.com>
2017-12-10 13:40:41 +0100

    added tostring for RecipeIngredience, get Ingredience importance by id, get all ingredience ids, counting of recipes for specific ingredience

--- Recipes/Models/RecipeIngredient.cs
+++ Recipes/Models/RecipeIngredient.cs
@@ -13,5 +13,16 @@
         public string Measure { get; set; }
 
         public Ingredient Ingredient { get; set; }
+
+        public override string ToString()
+        {
+            var stringBuilder = new System.Text.StringBuilder();
+            stringBuilder.AppendLine($"IngredientId: {Ingredient.Id}");
+            stringBuilder.AppendLine($"Name: {Name}");
+            stringBuilder.AppendLine($"Quantity:  {Quantity}");
+            stringBuilder.AppendLine($"Measure: {Measure}");
+
+            return stringBuilder.ToString();
+        }
     }
 }
